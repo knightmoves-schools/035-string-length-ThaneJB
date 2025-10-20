4d866da29d414e56308b162f071babffebb69b25
Thane <thabay0606@students.maquoketaschools.org>
2025-10-20 15:58:21 -0500

    Added quote length variable, twenty second letter variable, and output variable.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
+var quoteLength = quote.length;
+var twentySecondLetter = quote[21];
+var output = '${quoteLength}, [ ${twentySecondLetter}' ];
+document.getElementById("result").innerHTML = output;

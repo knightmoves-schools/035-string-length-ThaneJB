384872fd328a85227198c7a62114d0faa7890cc1
Thane <thabay0606@students.maquoketaschools.org>
2025-10-20 17:23:00 -0500

    Testing continues

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let quote = "...ask not what your country can do for you — ask what you can do
 //Add code below
 var quoteLength = quote.length;
 var twentySecondLetter = quote[21];
-var output = `${quoteLength}`;
-var output = `[${twentySecondLetter}]`;
+var output = `${twentySecondLetter}`;
+
+
 document.getElementById("result").innerHTML = output;
